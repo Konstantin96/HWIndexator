7d59eee1aca3011a3c324dd9dbd1b48c79ea85ff
Konstatin <34293528+Konstantin96@users.noreply.github.com>
2018-07-10 20:50:02 +0600

    Добавьте файлы проекта.

---
+++ HWIndexator/Program.cs
@@ -0,0 +1,28 @@
+﻿using MyArray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWIndexator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            MyArr array = new MyArr(3);
+            array[0] = 4;
+            array[1] = 134;
+            array[2] = 1000;
+
+            for (int i = 0; i < array.Size; i++)
+            {
+                Console.Write("{0}. {1} * {2} = {3}",i+1,Math.Sqrt(array.GetElem(i)), Math.Sqrt(array.GetElem(i)), array[i]);
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
---
+++ MyArray/Array.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArray
+{
+    public class MyArr
+    {
+        private int[] _array;
+
+        public MyArr()
+        {
+            Size = 1;
+            _array = new int[Size];
+        }
+
+        public MyArr(int size)
+        {
+            Size = size;
+            _array = new int[Size];
+        }
+
+        public int Size { get; set; }
+        public int GetElem(int si)
+        {
+            return _array[si];
+        }
+        public int this[int i]
+        {
+            get
+            {
+                return _array[i];
+            }
+            set
+            {
+                _array[i] = (int)Math.Pow((double)value, 2);
+            }
+        }
+
+    }
+}
